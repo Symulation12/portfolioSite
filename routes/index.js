4ecff5c9285eff411bf0696cbeff08beeeefd803
Sym <Symulation12@gmail.com>
2021-03-24 19:15:19 -0700

    Added pure css, made a test header

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Symsarchive.info' });
+router.get('/', function(req, res) {
+  res.render('index');
 });
+
+/*router.get('/projects', function(req, res) {
+  res.render('projects');
+});*/
 
 module.exports = router;
