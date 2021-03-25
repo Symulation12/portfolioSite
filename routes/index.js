11edc9d1bb335e1b4dd944d282da3ce48a1467fb
Sym <Symulation12@gmail.com>
2021-03-25 13:57:02 -0700

    Implmented a layout for streamlining production of project pages

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', {title: "Sym's Archive"});
 });
 
-/*router.get('/projects', function(req, res) {
-  res.render('projects');
-});*/
+router.get('/projects/*', function(req, res) {
+  var project = req.originalUrl.split('/').slice(-1).pop();
+  res.render(`projects/${project}`, {title: project});
+  
+});
 
 module.exports = router;
